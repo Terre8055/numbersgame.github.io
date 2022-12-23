a4adaff9e8575d1e192c06bb6dad937f22508a84
Terre8055 <michaelappiah2018@icloud.com>
2022-12-23 21:21:05 +0000

    learning typescript

---
+++ dist/demo.js
@@ -0,0 +1,12 @@
+"use strict";
+function greetPerson(name) {
+    if (name === "Mike") {
+        var greet = "Hello Mike";
+    }
+    else {
+        var greet = "Hi there";
+    }
+    console.log(greet);
+}
+greetPerson("Mike");
+//# sourceMappingURL=demo.js.map
---
+++ src/demo.ts
@@ -0,0 +1,10 @@
+function greetPerson(name: string){
+    if(name === "Mike"){
+        var greet = "Hello Mike";
+    }
+    else{
+        var greet = "Hi there";
+    }
+    console.log(greet);
+}
+greetPerson("Mike");
